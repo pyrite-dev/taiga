61378d315b8461e16423d404543fb4b57fbbd2ea
NishiOwO <nishi@nishi.boats>
2026-03-01 23:36:47 +0900

    oops

---
+++ src/util.c
@@ -0,0 +1,22 @@
+#include <taiga.h>
+
+char* u_strvacat(const char* str, ...){
+	va_list va;
+	int len = 0;
+	const char* s;
+	char* r;
+
+	len += strlen(str);
+	va_start(va, str);
+	while((s = va_arg(va, const char*)) != NULL) len += strlen(s);
+	va_end(va);
+
+	r = malloc(len + 1);
+
+	strcpy(r, str);
+	va_start(va, str);
+	while((s = va_arg(va, const char*)) != NULL) strcat(r, s);
+	va_end(va);
+
+	return r;
+}
